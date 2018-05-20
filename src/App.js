64c8e475f8fbcd927451ccf43b2c716650c77e2b
Lokesh Khatri <lokesh.a2z@gmail.com>
2018-05-20 18:38:54 +0100

    feat(App): integrated Redux and all basic functionality works now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import AddBookmark from './containers/AddBookmark';
+import BookmarkListContainer from './containers/BookmarkListContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AddBookmark/>
+        <BookmarkListContainer/>
       </div>
     );
   }
